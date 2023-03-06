05c4b4cfa1fa287b114dbdc8d702e7920173ec64
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-06 12:02:58 +0000

    part 1 done

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,47 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
+    this.itemsShortened = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
     const item = { id: this.id, text: str, status: 'incomplete' }
     this.items.push(item)
     return item
   }
 
-  showAll () {
-    return this.items
+  showAll() {
+    // console.log('HERE', this.items)
+    const newList = this.items
+    newList.forEach((toDoItem) => {
+      if (toDoItem.text.length > 20) {
+        toDoItem.text = toDoItem.text.substring(0, 20)
+        toDoItem.text += '...'
+      }
+    })
+    this.itemsShortened = newList
+    return newList
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
